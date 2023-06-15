4c4bed6a12289eb7e257f90c7c11bd9f15b97a72
M-Aimar <90445954+M-Aimar@users.noreply.github.com>
2023-06-15 11:22:35 +0200

    updated lemigo hotel

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Raleway: ['Raleway'],
+				Cinzel: ['Cinzel'],
+
+			}
+		},
+	},
+	plugins: [],
+}
